a8d8e4a5e2670c521a99feadfbb766791ffab0df
surajondev <surajvishwakarma625@gmail.com>
2022-05-21 22:39:58 +0530

    adding sitemap

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -1,8 +1,14 @@
 exports.onCreateWebpackConfig = args => {
   args.actions.setWebpackConfig({
-    siteMetadata: {
-        siteUrl: `https://web3-roadmap.netlify.app`,
-      },
-      plugins: [`gatsby-plugin-sitemap`],
-    }
+    // siteMetadata: {
+    //     siteUrl: `https://web3-roadmap.netlify.app`,
+    //   },
+    //   plugins: [`gatsby-plugin-sitemap`],
+    // }
+    module :{
+        siteMetadata: {
+            siteUrl: `https://www.example.com`,
+        }
+    }, 
+    plugins: [`gatsby-plugin-sitemap`]
 )}
