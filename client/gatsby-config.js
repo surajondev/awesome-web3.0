6998160d57338e29c93e9d3d9f5e8a8851554f93
surajondev <surajvishwakarma625@gmail.com>
2022-08-17 09:36:31 +0530

    adding sitemap

---
+++ client/gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    siteMetadata: {
+        // If you didn't use the resolveSiteUrl option this needs to be set
+        siteUrl: `https://web3-roadmap.netlify.app`,
+      },
+      plugins: [`gatsby-plugin-sitemap`]
+    }
--- client/gatsby-node.js
+++
@@ -1,9 +0,0 @@
-exports.onCreateWebpackConfig = args => {
-  args.actions.setWebpackConfig({
-    module :{
-        siteMetadata: {
-            siteUrl: `https://web3-roadmap.netlify.app/`,
-        }
-    }, 
-    plugins: [`gatsby-plugin-sitemap`]
-})}
